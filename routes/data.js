49087c33223fbba8bf6ce176bea3684a023c7913
yoav <yoav.broza@gmail.com>
2022-03-30 12:00:05 +0300

    data actions

--- routes/data.js
+++ routes/data.js
@@ -1,2 +1,55 @@
+const { Accounts } = require('../mongo/account-model')
+
 const router = require('express').Router()
-module.exports =router
+
+/**
+ * @openapi
+ * /api/data/history:
+ *   get:
+ *     description: returns account's actions  history.
+ *     tags: [Data]
+ *     responses:
+ *       200:
+ *         description: OK.
+ *       400:
+ *         description: please login first.
+ *     
+ */
+router.get('/history', async (req, res) => {
+    try {
+        const account = await Accounts.findById(req.session.account.id).populate({
+            path: 'actions.to',
+            select: 'username'
+        }).populate({
+            path: 'actions.from',
+            select: 'username'
+        })
+        res.send({ history: account.actions, balance: account.balance })
+    } catch (err) {
+        res.status(500).send({ err: err })
+        console.log(err)
+    }
+})
+/**
+ * @openapi
+ * /api/data/accounts:
+ *   get:
+ *     description: returns accounts search result.
+ *     tags: [Data]
+ *     responses:
+ *       200:
+ *         description: OK.
+ *       400:
+ *         description: please login first.
+ *     
+ */
+router.get('/accounts', async (req, res) => {
+    try {
+        const accounts = await Accounts.find({}, { username: 1 })
+        res.send(accounts)
+    } catch (error) {
+        res.status(500).send({ err: err })
+        console.log(err)
+    }
+})
+module.exports = router
